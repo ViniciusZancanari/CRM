bdd694962bf4654d9a4f552bc6a070941baf7e65
ViniciusZancanari <viniciuszanca@hotmail.com>
2023-05-03 17:21:17 -0300

    add/Proposal Table

---
+++ src/componentes/Proposal.js
@@ -0,0 +1,15 @@
+function Proposal() {
+    return (
+        <table>
+            <thead>Propostas</thead>
+            <tr>
+            <td>1</td>
+            </tr>
+            <tr>
+                <td>2</td>
+            </tr>
+        </table>
+    )
+}
+
+export default Proposal;
