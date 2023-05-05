8c32ac3a5dd3a107be05cab1859c5108029f4b6c
ViniciusZancanari <viniciuszanca@hotmail.com>
2023-05-05 16:24:59 -0300

    add/router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import Consult from './componentes/Consult';
-import Proposal from './componentes/TableProposal';
-import Register from './componentes/Register';
+import Consult from './componentes/pages/Consult';
+import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+import Login from './componentes/pages/Login';
 
 function App() {
+/*
+function Layout() {
+  Para colocar mais de um componente dentro de um router, utilize
+  o Layout 
 
-  
-  
+  return(
+    <compente1/>
+    <compente2/>
+  )
+*/
   return (
     <div className="App">
-     <Consult/>
+      <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<Login />} />
+            <Route path="/consult" element={<Consult />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
