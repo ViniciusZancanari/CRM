6fd7aa8116ecdb9afe1e114705f0647e6c3a1f0c
ViniciusZancanari <viniciuszanca@hotmail.com>
2023-05-09 16:59:35 -0300

    add/rotas

---
+++ src/componentes/Rotas.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+import Consult from '../componentes/pages/Consult';
+import Register from '../componentes/pages/Register';
+import Login from '../componentes/pages/Login';
+import Edit from '../componentes/pages/Edit';
+
+function Rotas() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={<Login />} />
+                <Route path="/consult" element={<Consult />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/edit" element={<Edit />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Rotas;
