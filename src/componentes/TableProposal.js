488d38e27fae306ba9541b4c731c0f78b9ca5f56
ViniciusZancanari <viniciuszanca@hotmail.com>
2023-05-09 16:38:16 -0300

    ref/table proposal

--- src/componentes/TableProposal.js
+++ src/componentes/TableProposal.js
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function TableProposal({propostas}) {
-  //<Link to="/register">{proposta.companyName}</Link>
 
-    console.log(propostas)
-    return (
-        <table>
-        {propostas.map((proposta, index) => (
-         <button>
+
+function TableProposal({ propostas }) {
+  const [ultimoClicado, setUltimoClicado] = useState(null);
+
+  function handleClick(event) {
+    setUltimoClicado(event.target);
+    console.log(event.target)
+  }
+
+
+  //console.log(propostas)
+  return (
+    <table>
+      {propostas.map((proposta, index) => (
+        <button onClick={handleClick}>
           <tr key={index}>
             <p>Nº SGSET: {proposta.SGSETNumber}</p>
             <p>Status: {proposta.status}</p>
+            <Link to="/edit">Editar </Link>
           </tr>
-          
-          </button > 
-        ))}
-      </table>
-    )
+        </button >
+      ))}
+    </table>
+  )
 }
 
 export default TableProposal;
