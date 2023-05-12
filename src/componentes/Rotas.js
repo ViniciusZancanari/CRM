6c3c0e35c2aeaf33ed8b50150e7e44c74b659ed5
ViniciusZancanari <viniciuszanca@hotmail.com>
2023-05-12 15:28:41 -0300

    add/rotas - menu

--- src/componentes/Rotas.js
+++ src/componentes/Rotas.js
@@ -3,6 +3,7 @@ import Consult from '../componentes/pages/Consult';
 import Register from '../componentes/pages/Register';
 import Login from '../componentes/pages/Login';
 import Edit from '../componentes/pages/Edit';
+import Menu from './pages/Menu';
 
 function Rotas() {
     return (
@@ -13,6 +14,8 @@ function Rotas() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/edit" element={<Edit />} />
                 <Route path="/edit/:SGSETNumber" element={<Edit />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="*" element={<h1>Page not found</h1>} />
             </Routes>
         </BrowserRouter>
     )
