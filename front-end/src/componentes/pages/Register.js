04930e05e423fc32fe7936531bbfb4debfba351e
ViniciusZancanari <viniciuszanca@hotmail.com>
2023-08-24 16:36:24 -0300

    add/register - styles

--- front-end/src/componentes/pages/Register.js
+++ front-end/src/componentes/pages/Register.js
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Axios from 'axios'
-import styles from '../../index.css'
-
+import './index.css'
 
 
 function Teste() {
 
     const [values, setValues] = useState();
     const { handleSubmit } = useForm();
-
-
-    /*useEffect (()=>
-    {
-        Axios.get("http://localhost:3001/getPropostal").then((response)=>{
-            console.log(response)
-        })
-    },[])
-    */
 
     const handleaddValues = (value) => {
         setValues((prevValues) => ({
@@ -61,158 +51,170 @@ function Teste() {
     }
 
 
-
-
     return (
 
         <form onSubmit={handleSubmit(onSubmit)}>
 
             <div className="register">
                 <h1>Cadastrar Novo Atendimento</h1>
-                <label>CNPJ</label>
-                <input
-                    type="text"
-                    name="idCNPJ"
-                    onChange={handleaddValues}
-                />
 
-                <label>Razão Social</label>
-                <input
-                    type="text"
-                    name="razaoSocial"
-                    onChange={handleaddValues}
-                    placeholder='Gerado por API'
-                />
+                <div className="NovoAtendimeto">
+                    <label>Razão Social</label>
+                    <input
+                        type="text"
+                        name="razaoSocial"
+                        onChange={handleaddValues}
+                        placeholder='Gerado por API'
+                    />
 
-                <label>Porte</label>
-                <input
-                    type="text"
-                    name="porte"
-                    onChange={handleaddValues}
-                    placeholder='Gerado por API'
-                />
+                    <label>CNPJ</label>
+                    <input
+                        type="text"
+                        name="idCNPJ"
+                        onChange={handleaddValues}
+                    />
 
-                <label>Cidade</label>
-                <input
-                    type="text"
-                    name="cidade"
-                    onChange={handleaddValues}
-                    placeholder='Gerado por API'
-                />
-                <label>Estado</label>
-                <input
-                    type="text"
-                    name="estado"
-                    onChange={handleaddValues}
-                    placeholder='Gerado por API'
-                />
+                    <label>Porte</label>
+                    <input
+                        type="text"
+                        name="porte"
+                        onChange={handleaddValues}
+                        placeholder='Gerado por API'
+                    />
+                </div>
 
-                <label>Pais</label>
-                <input
-                    type="text"
-                    name="pais"
-                    onChange={handleaddValues}
-                    placeholder='Gerado por API'
-                />
+                <div>
 
-                <h2>Contato da Empresa</h2>
-                <label>Nome</label>
-                <input
-                    type='text'
-                    name="nome"
-                    onChange={handleaddValues}
-                ></input>
+                    <label>Cidade</label>
+                    <input
+                        type="text"
+                        name="cidade"
+                        onChange={handleaddValues}
+                        placeholder='Gerado por API'
+                    />
+                    <label>Estado</label>
+                    <input
+                        type="text"
+                        name="estado"
+                        onChange={handleaddValues}
+                        placeholder='Gerado por API'
+                    />
 
-                <label>Telefone</label>
-                <input
-                    type='text'
-                    name="telefone"
-                    onChange={handleaddValues}
-                ></input>
+                    <label>Pais</label>
+                    <input
+                        type="text"
+                        name="pais"
+                        onChange={handleaddValues}
+                        placeholder='Gerado por API'
+                    />
+                </div>
 
-                <label>E-mail</label>
-                <input
-                    type='text'
-                    name="email"
-                    onChange={handleaddValues}
-                ></input>
+                <div>
+                    <h1>Contato da Empresa</h1>
+                    <label>Nome</label>
+                    <input
+                        type='text'
+                        name="nome"
+                        onChange={handleaddValues}
+                    ></input>
 
-                <h3>Dados da Proposta</h3>
-                <label>Número da Proposta SGSET</label>
-                <input
-                    type='text'
-                    name="numeroSGSET"
-                    onChange={handleaddValues}
-                    placeholder='Número da Proposta SGSET'
-                />
+                    <label>Telefone</label>
+                    <input
+                        type='text'
+                        name="telefone"
+                        onChange={handleaddValues}
+                    ></input>
 
-                <label>Data</label>
-                <input
-                    type='date'
-                    name="data"
-                    onChange={handleaddValues}
-                    placeholder='Número da Proposta SGSET'
-                />
+                    <label>E-mail</label>
+                    <input
+                        type='text'
+                        name="email"
+                        onChange={handleaddValues}
+                    ></input>
+                </div>
+
+                <div>
+                    <h1>Dados da Proposta</h1>
+                    <label>Número da Proposta SGSET</label>
+                    <input
+                        type='text'
+                        name="numeroSGSET"
+                        onChange={handleaddValues}
+                        placeholder='Número da Proposta SGSET'
+                    />
+
+                    <label>Assunto</label>
+                    <input
+                        type='text'
+                        name="assunto"
+                        onChange={handleaddValues}
+                    />
+
+                    <label>Data</label>
+                    <input
+                        type='date'
+                        name="data"
+                        onChange={handleaddValues}
+                        placeholder='Número da Proposta SGSET'
+                    />
+                </div>
 
 
-                <label>Assunto</label>
-                <input
-                    type='text'
-                    name="assunto"
-                    onChange={handleaddValues}
-                />
+                <div>
+                    <label>Fomento</label>
+                    <input
+                        type='text'
+                        name="fomento"
+                        onChange={handleaddValues}
+                    />
 
-                <label>Fomento</label>
-                <input
-                    type='text'
-                    name="fomento"
-                    onChange={handleaddValues}
-                />
+                    <label>Responsável Comercial</label>
+                    <input
+                        type='text'
+                        name="responsavelComercial"
+                        onChange={handleaddValues}
+                        placeholder='Op'
+                    />
 
-                <label>Responsável Comercial</label>
-                <input
-                    type='text'
-                    name="responsavelComercial"
-                    onChange={handleaddValues}
-                    placeholder='Op'
-                />
+                    <label>Responsável Técnico</label>
+                    <input
+                        type='text'
+                        name="responsavelTecnico"
+                        onChange={handleaddValues}
+                        placeholder='Op'
+                    />
 
-                <label>Responsável Técnico</label>
-                <input
-                    type='text'
-                    name="responsavelTecnico"
-                    onChange={handleaddValues}
-                    placeholder='Op'
-                />
+                    <label>Área</label>
+                    <input
+                        type='text'
+                        name="area"
+                        onChange={handleaddValues}
+                        placeholder='Op'
+                    />
+                </div>
+                <div>
+                    <label>Preço</label>
+                    <input
+                        type="text"
+                        name="preco"
+                        onChange={handleaddValues}
+                        placeholder='R$'
+                    />
 
-                <label>Área</label>
-                <input
-                    type='text'
-                    name="area"
-                    onChange={handleaddValues}
-                    placeholder='Op'
-                />
-                <label>Preço</label>
-                <input
-                    type="text"
-                    name="preco"
-                    onChange={handleaddValues}
-                    placeholder='R$'
-                />
+                    <label>Status</label>
+                    <input
+                        type="text"
+                        name="status"
+                        onChange={handleaddValues}
+                    />
 
-                <label>Status</label>
-                <input
-                    type="text"
-                    name="status"
-                    onChange={handleaddValues}
-                />
-
-                <label>Parcela Guarda-Chuva:</label>
-                <input
-                    type="text"
-                    name="guardaChuva"
-                    onChange={handleaddValues}
-                />
+                    <label>Parcela Guarda-Chuva:</label>
+                    <input
+                        type="text"
+                        name="guardaChuva"
+                        onChange={handleaddValues}
+                    />
+                </div>
                 <button type="submit">OK</button>
 
             </div>
